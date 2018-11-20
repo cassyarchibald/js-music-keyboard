72c6230d8f7ffff97d8a7b6e585d685de5c163c7
Cassandra Archibald <cassandra.l.archibald@gmail.com>
2018-11-19 16:47:40 -0800

    Added keypress to keyboard

--- noise.js
+++ noise.js
@@ -3,9 +3,32 @@ $(document).ready(function() {
   // Use event delegation? audio_embeds .click(function(event){
   // get class via event / do class.play()?
   // })
+  $(document).keypress(function(event) {
+    console.log(event.key);
+    if (event.key === "a") {
+      $("#aAudio")[0].play();
+    } else if (event.key === "b") {
+      $("#bAudio")[0].play();
+    } else if (event.key === "c") {
+      $("#cAudio")[0].play();
+    } else if (event.key === "d") {
+      $("#dAudio")[0].play();
+    } else if (event.key === "e") {
+      $("#eAudio")[0].play();
+    } else if (event.key === "f") {
+      $("#fAudio")[0].play();
+    } else if (event.key === "g") {
+      $("#gAudio")[0].play();
+    }
+  });
+  // document.onkeydown(function(event) {
+  //   console.log("CLICKED");
+  //   console.log(event.keyCode);
+  // });
   $(".instrument").on("click", "button", function(event) {
     // Swith case for class?
     if ($(event.target).hasClass("a")) {
+      console.log($(event.target.key));
       $("#aAudio")[0].play();
     } else if ($(event.target).hasClass("b")) {
       $("#bAudio")[0].play();
